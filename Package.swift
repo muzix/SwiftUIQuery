31900c89593eb35700955316f1ebbec8f8209c00
Hoang Pham <hoangpham@swiftcode.me>
2025-08-17 18:29:57 +0700

    chore: support macos v12

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "SwiftUIQuery",
     platforms: [
         .iOS(.v16),
-        .macOS(.v13),
+        .macOS(.v12),
         .tvOS(.v16),
         .watchOS(.v9)
     ],
